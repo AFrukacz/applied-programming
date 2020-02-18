2f49be0ed04434cc81a1a2e169c49e087efd7a38
AFrukacz <afrukacz@gmail.com>
2020-02-18 02:25:08 +0000

    Merge branch 'master' of https://github.com/AFrukacz/applied-programming

--- dice.rb
+++ dice.rb
@@ -4,9 +4,12 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-    "<h1>Hello, world! </h1>"
+    @die1 = rand(1..6)
+    @die2 = rand(1..6)
+    "<h1>#{@die1} #{@die2}</h1>"
+    view "dice"
 end
 
-get "/tacos" do
-    "<h1>Yes, please!</h1>"
-end
+#get "/tacos" do
+#   "<h1>Yes, please!</h1>"
+#end
