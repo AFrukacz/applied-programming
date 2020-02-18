c9625f49481cff1dcbd969dd472e1cd84c1717a2
AFrukacz <afrukacz@gmail.com>
2020-02-18 02:37:23 +0000

    Updated for Maps

--- where2.rb
+++ where2.rb
@@ -4,6 +4,11 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
+    @lat = rand(-90.0..90.0)
+    @long = rand(-180.0..180.0)
+    "<h1>#{@lat} #{@long}</h1>"
+    view "where2"
   # lat: ± 90
   # long: ± 180
+  
 end
